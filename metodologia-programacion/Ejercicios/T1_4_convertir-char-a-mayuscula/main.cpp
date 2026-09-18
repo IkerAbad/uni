9d4109b1e5d60379e12b14f97ff1609da20c5e83
IkerAbad <75870291+IkerAbad@users.noreply.github.com>
2026-09-18 19:31:02 +0200

    Ejercicio_T1_4_convertir-char-a-mayuscula

---
+++ metodologia-programacion/Ejercicios/T1_4_convertir-char-a-mayuscula/main.cpp
@@ -0,0 +1,27 @@
+/*
+    FECHA: 18/09/2026
+    AUTOR: Iker
+    DESCRIPCIÓN:
+        Escribe un programa que, dada una letra minúscula muestre por pantalla la correspondiente letra mayúscula.
+    ENTRADAS: letra minúscula
+    SALIDAS: letra mayúscula
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char minuscula;
+    int mayuscula;
+
+    cout << "Introduce una letra minúscula:" << endl;
+    cin >>minuscula;
+
+//convertir a mayúscula
+    mayuscula = int(minuscula) - 32;
+
+//muestra los resultados
+    cout << char(mayuscula) << endl;
+
+    return 0;
+}
