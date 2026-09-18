907bad4dcf48e2ceee0f4031745e4da605f60f15
IkerAbad <75870291+IkerAbad@users.noreply.github.com>
2026-09-18 07:52:13 +0200

    Create main.cpp

---
+++ metodologia-programacion/Ejercicios/T1_prueba/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int base=5, altura=7;
+    int area;
+
+    area = base * altura;
+    cout << "El área es: " << area;
+    return 0;
+}
