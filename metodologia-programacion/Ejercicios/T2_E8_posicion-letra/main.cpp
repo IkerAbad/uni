513d15eb13f2db08d3c7ae6b0cb0e8707c317925
IkerAbad <75870291+IkerAbad@users.noreply.github.com>
2026-09-19 13:35:51 +0200

    Ejercicio_T2_8_posicion-letra

---
+++ metodologia-programacion/Ejercicios/T2_E8_posicion-letra/main.cpp
@@ -0,0 +1,29 @@
+/*
+    FECHA: 19/09/2026
+    AUTOR: Iker
+    DESCRIPCIÓN:
+        Leer una letra mayúscula y escribir su posición en el abecedario
+    ENTRADAS: letra (char)
+    SALIDAS: posicion (int)
+    ERRORES: La letra introducida no es mayúscula y si la letra no pertenece al abecedario anglosajón.
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char letra;      //variable entrada
+    int posicion;   //variable salida
+    const int desplazamiento = int('A') - 1;   // 'A' vale 65; A -> posición 1
+
+//teclado
+    cout << "Introduce una letra mayúscula: ";
+    cin >> letra;
+
+//posición
+    posicion = int(letra) - desplazamiento;
+
+//muestra los resultados
+    cout << "Posición en el abecedario: " << posicion;
+    return 0;
+}
