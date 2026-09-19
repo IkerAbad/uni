8f16e4599841df7cc18994f1c960b0bd974ab287
IkerAbad <ikerfernandezanton@gmail.com>
2026-09-19 21:59:12 +0200

    P2 plantilla estructura basica

---
+++ metodologia-programacion/Ejercicios/T2_P1_Estructura-basica/main.cpp
@@ -0,0 +1,18 @@
+/*
+#include <nombre_de_fichero>            //directivas al preprocesador
+
+using namespace std;
+
+int main()                              // inicio del bloque principal
+{
+    const tipo_dato cte_1 = valor_1;    // definiciones de constantes
+    tipo_1 var_1;                       // declaraciones de variables
+
+    tipo_2 var_2, var_3;
+    ...                                 // cuerpo del bloque principal
+    ...
+}
+
+// definiciones de funciones
+
+*/
