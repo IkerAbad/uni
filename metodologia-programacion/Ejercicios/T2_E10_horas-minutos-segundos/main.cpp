9515915d7b8dbf7d8459298729e936a4ca8a9088
IkerAbad <75870291+IkerAbad@users.noreply.github.com>
2026-09-19 17:02:27 +0200

    Ejercicio_T2_10_horas-minutos-segundos

---
+++ metodologia-programacion/Ejercicios/T2_E10_horas-minutos-segundos/main.cpp
@@ -0,0 +1,31 @@
+/*
+    FECHA: 19/09/2026
+    AUTOR: Iker
+    DESCRIPCIÓN:
+        Conversión de una cantidad positiva de segundos a su equivalente en horas, minutos y segundos
+    ENTRADAS: segundos (int)
+    SALIDAS: horas (int), minutos (int), segundos (int)
+    ERRORES: --
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int segundos1;      //variable entrada
+    int horas,minutos,segundos2;       //variables salida
+
+    cout << "Introduce número de segundos: ";
+    cin >> segundos1;
+
+//cálculo
+    horas = segundos1 / 3600;
+    minutos = (segundos1 - (horas * 3600)) / 60;
+    segundos2 = (segundos1 - (horas * 3600)) - (minutos * 60);
+
+//muestra los resultados
+    cout << "Horas: " << horas << endl;
+    cout << "Minutos: " << minutos << endl;
+    cout << "Segundos: " << segundos2 << endl;
+    return 0;
+}
